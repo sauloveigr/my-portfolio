db684fbb0c8212de60d8fe152a3e7722d6edd190
Saulo Veiga <sauloveigr@hotmail.com>
2025-01-13 13:23:56 -0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,157 @@
+export const navItems = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Building Innovative Projects in Real-Time.",
+    description: "Live-Coding Twitch Streamer",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    title: "Form",
+    description:
+      "A responsive form built with React, Formik, Styled Components and Material-UI.",
+    img: "/project4.png",
+    link: "https://sauloveigr.github.io/form/",
+  },
+  {
+    title: "Spirited Away",
+    description:
+      "A responsive landing page built with React and Styled Components.",
+    img: "/project3.png",
+    link: "https://sauloveigr.github.io/studio-ghibli-react/",
+  },
+  {
+    title: "Figma Land",
+    description:
+      "A landing page built with React, Styled Components and React Router DOM.",
+    img: "/project1.png",
+    link: "https://sauloveigr.github.io/landing-page-react/",
+  },
+  {
+    title: "NFT Rocket",
+    description: "A landing page built with React and Styled Components.",
+    img: "/project2.png",
+    link: "https://sauloveigr.github.io/rocket-nft-react/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Saulo is a skilled and committed engineer. I had the opportunity to work directly with Saulo on several projects and was always impressed with his contributions. He is glue to any team and always resilient and poised when pressure is high. I highly recommend Saulo and am happy to speak directly to his work.",
+    name: "Eric Kramlinger",
+    title: "Founder @ LMP",
+    picture: "/eric.png",
+  },
+  {
+    quote:
+      "Saulo is an excellent professional, always delivering concise and well-executed work. He is dedicated to his craft and consistently seeks to improve and provide the best possible solutions. Highly recommend!",
+    name: "Talita Lima",
+    title: "Senior Quality Assurance Engineer",
+    picture: "/talita.png",
+  },
+  {
+    quote:
+      "Saulo is a dedicated developer and great person to work with. His knowledge can help a lot on a development team.",
+    name: "Yauari Vieira",
+    title: "Senior Frontend Engineer",
+    picture: "/yauari.png",
+  },
+  {
+    quote:
+      "Saulo is a highly skilled developer. I had the pleasure of working with him for over a year and witnessed a remarkable improvement in his abilities. He consistently delivers quality work and is always open to feedback and eager to learn new things. I assisted him with some questions and new learning opportunities, and he always responded with outstanding results. Saulo is an excellent professional, both technically and personally. He is communicative and easy-going, making it a pleasure to work with him. It was a wonderful experience to witness his growth.",
+    name: "Adriano Maringolo",
+    title: "Senior Frontend Engineer",
+    picture: "/adriano.png",
+  },
+];
+
+export const workExperience = [
+  {
+    title: "Frontend Engineer",
+    desc: "Worked at a multinational company with international teams, developing solutions, enhancing the platform, and resolving bugs for large enterprise clients.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    title: "Freelance Dev Project",
+    desc: "Assisted in the development of a website for a Canadian client, enhancing the product and creating new features.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    img: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/saulosrv/",
+  },
+  {
+    img: "/github.svg",
+    link: "https://github.com/sauloveigr",
+  },
+];
