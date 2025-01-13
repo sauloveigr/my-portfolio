042bcdb6face23b9d3ac7603e51ab36f54918b1c
Saulo Veiga <sauloveigr@hotmail.com>
2025-01-13 16:08:27 -0300

    feat: hide navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { navItems } from "@/data";
+// import { FloatingNav } from "@/components/ui/FloatingNavbar";
+// import { navItems } from "@/data";
 
 import dynamic from "next/dynamic";
 //@ts-ignore
@@ -20,7 +20,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       {
         <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems} />
+          {/* <FloatingNav navItems={navItems} /> */}
           <Hero />
           <Grid />
           <RecentProjects />
